d8c788dbd5bca4f17d1f9dc86dd7d0b2829117e4
Adeen <adeenkhoso@gmail.com>
2023-12-24 01:19:48 +0500

    worked on hero section

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    // initializing elements 
+  // initializing elements
+  const navdiv = document.getElementById("nav");
   const navigation = document.getElementById("navigation");
   const hamburger = document.getElementById("hamburger");
   const branding = document.getElementById("branding");
-  const navLinks = document.querySelectorAll('.navigation a');
+  const navLinks = document.querySelectorAll(".navigation a");
 
   hamburger.onclick = function () {
     navigation.classList.toggle("active");
+    hamburger.classList.toggle("active");
     branding.classList.toggle("active");
-    hamburger.classList.toggle("is-active");
   };
 
   navLinks.forEach(function (link) {
-    link.addEventListener('click', function () {
-      navigation.classList.remove('active');
-      branding.classList.remove('active');
+    link.addEventListener("click", function () {
+      navigation.classList.remove("active");
+      branding.classList.remove("active");
     });
   });
+
+  window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    const targetDiv = document.querySelector('#target-div');// need to put the name of the div where the nav will change its color !
+  
+    if (scrollPosition > 50 || targetDiv.getBoundingClientRect().top >= 0) {
+      nav.classList.add("after-scroll");   
+    } else {
+      nav.style.display = 'none';
+    }
+  });
+
 });
