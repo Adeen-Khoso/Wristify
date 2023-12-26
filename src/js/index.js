993d6ec46ed7fe13a4be9ed8afb614095500f17c
Adeen <adeenkhoso@gmail.com>
2023-12-26 22:44:37 +0500

    about-section/home completed

--- src/js/index.js
+++ src/js/index.js
@@ -50,20 +50,22 @@
 
 
   // intersection observer api for animation events
-  const observer = new IntersectionObserver((entries)=>{
+  const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.isIntersecting){
-        const animationClass = entry.target.classList.contains('feature')
-        ? 'feature-animate'
-        : 'animate';  
-        entry.target.classList.add(animationClass)
-      }else{
-        entry.target.classList.remove('animate','feature-animate');
+      if (entry.isIntersecting) {
+        const animationClass =
+          entry.target.classList.contains('feature')
+            ? 'feature-animate'
+            : entry.target.classList.contains('btn')
+            ? 'btn-animation' 
+            : 'animate';
+        entry.target.classList.add(animationClass);
+      } else {
+        entry.target.classList.remove('animate', 'feature-animate', 'btn-animation');
       }
     });
   });
-
+  
 // for animations
   const hiddenElements = document.querySelectorAll(".hidden")
   hiddenElements.forEach((el)=> observer.observe(el));
@@ -72,3 +74,5 @@
   const feature = document.querySelectorAll('.feature');
   feature.forEach((el)=> observer.observe(el));
 
+  const buttonElements = document.querySelectorAll(".btn");
+buttonElements.forEach((el) => observer.observe(el));
